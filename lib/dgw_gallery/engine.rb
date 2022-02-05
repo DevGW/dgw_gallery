ee7070af21894303751921d06d604232ddb134c2
Jason Becht <jason@cryptechstudios.com>
2022-02-05 14:59:03 -0600

    image stuff

--- lib/dgw_gallery/engine.rb
+++ lib/dgw_gallery/engine.rb
@@ -1,4 +1,8 @@
 module DgwGallery
   class Engine < ::Rails::Engine
+    initializer "blorgh.assets.precompile" do |app|
+        app.config.assets.precompile += %w( no_image.png )
+    end
+
   end
 end
