c38daa6a4f7e686dd23f1b6fe24ce5b0fd93247a
Jason Becht <jason@cryptechstudios.com>
2022-02-05 14:24:07 -0600

    5

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 #   put "gallery/add_to_gallery/:id", controller: 'gallery', action: 'add_to_gallery'
 #   put "gallery/remove_from_gallery/:id", controller: 'gallery', action: 'remove_from_gallery'
 #   put "gallery/upload", controller: 'gallery', action: 'upload_to_gallery'
-  root to: 'gallery#index'
+#   root to: 'gallery#index'
 end
--- lib/dgw_gallery/version.rb
+++ lib/dgw_gallery/version.rb
@@ -1,3 +1,3 @@
 module DgwGallery
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
 end
