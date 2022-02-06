089b1192e6bf68e097de6ac75e513ca04c8d2238
Jason Becht <jason@cryptechstudios.com>
2022-02-06 14:00:37 -0600

    ...

--- lib/dgw_gallery/engine.rb
+++ lib/dgw_gallery/engine.rb
@@ -1,7 +1,7 @@
 module DgwGallery
   class Engine < ::Rails::Engine
     initializer "dgw_gallery.assets.precompile" do |app|
-        app.config.assets.precompile += %w( no_image.png dgw_logo.png gallery.css )
+        app.config.assets.precompile += %w( no_image.png dgw_logo.png )
     end
 
   end
