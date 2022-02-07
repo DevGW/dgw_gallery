807f5e638d45a67ead58811dd68717320d72b6f6
Jason Becht <jason@cryptechstudios.com>
2022-02-07 12:24:24 -0600

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 #   resources :gallery
-    get 'gallery', controller: 'gallery', action: 'index'
     get 'gallery/new'
+    get 'gallery', controller: 'gallery', action: 'index'
     post 'gallery', controller: 'gallery', action: 'create'
     get 'gallery/manage/:id', controller: 'gallery', action: 'manage'
     put "gallery/add_to_gallery/:id", controller: 'gallery', action: 'add_to_gallery'
     put "gallery/remove_from_gallery/:id", controller: 'gallery', action: 'remove_from_gallery'
-    put "gallery/remove_gallery/:id", controller: 'gallery', action: 'remove_gallery'
+    put "gallery/remove/:id", controller: 'gallery', action: 'remove_gallery'
     put "gallery/upload", controller: 'gallery', action: 'upload_to_gallery'
 #   root to: 'gallery#index'
 end
